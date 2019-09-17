963c44799f63beba846684e8459685bc95c3e969
tracer-ics2019 <tracer@njuics.org>
2019-09-17 18:55:20 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-27-generic #28-Ubuntu SMP Tue Aug 20 19:53:07 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:55:20 up 1 day,  1:21,  1 user,  load average: 0.09, 0.16, 0.15 c5185b0d5b4cc8cfdfb03600d926ba03e1629874

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -157,7 +157,7 @@ uint32_t expr(char *e, bool *success) {
     return 0;
   }
   int i=0;
-  for(i=0;i<=nr_token;i++)
+  for(i=0;i<nr_token;i++)
 	printf("%s\n",tokens[i].str);
   printf("ljk::checkout  %d\n",check_parentheses(0,nr_token));
   init_tokens_ljk();
