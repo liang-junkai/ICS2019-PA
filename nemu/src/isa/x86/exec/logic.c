5299c0ac627969745b78184d8c104e21d51a240b
tracer-ics2019 <tracer@njuics.org>
2019-10-25 19:21:29 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:21:29 up  2:39,  1 user,  load average: 0.61, 0.85, 0.63 fb5136fa2ca1ab50b706ddfecc9a5de73f2d03cd

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -50,9 +50,10 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
-
+  rtl_shl(&s0,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&s0);
   print_asm_template2(shl);
 }
 
