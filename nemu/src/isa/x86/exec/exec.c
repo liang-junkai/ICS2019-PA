0ba2e3508b879bb2b9828730e293833f2c057dcb
tracer-ics2019 <tracer@njuics.org>
2019-10-29 17:37:55 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:37:55 up  2:54,  1 user,  load average: 0.78, 0.79, 0.82 df48a8769b03c767eebad8d5e873a011a95148b

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -70,7 +70,7 @@ make_group(gp4,
 
 /* 0xff */
 make_group(gp5,
-    EX(inc), EMPTY, IDEX(E,call), EX(call),
+    EX(inc), EMPTY, IDEX(E,call), IDEX(E,call),
     EMPTY, EMPTY, EX(push), EMPTY)
 
 /* 0x0f 0x01*/
