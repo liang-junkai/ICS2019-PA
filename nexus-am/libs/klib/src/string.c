9918ea3a0c1a9c8a08842e0d183cf52d71f143ab
tracer-ics2019 <tracer@njuics.org>
2019-10-30 19:37:41 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:37:41 up  4:32,  1 user,  load average: 0.91, 0.67, 0.46 7a8c87ff1c2824b6ecccbbd8dc62ae3faf13426b

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,5 +1,4 @@
 #include "klib.h"
-
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
@@ -20,14 +19,20 @@ char* strcat(char* dst, const char* src) {
 
 int strcmp(const char* s1, const char* s2) {
   while(*s1==*s2){
+    _putc(*s1);
+    _putc(' ');
+    _putc(*s2);
+    _putc('\n');
     if(*s1=='\0'||*s2=='\0')break;
     s1++;
     s2++;
-
   }
-  if(*s1-*s2<0)return -1;
-  return *s1-*s2; 
-  return 0;
+  if(*s1-*s2<0){
+    _putc('l');
+    return -1;
+    }
+    _putc('k');
+  return (int)(*s1-*s2);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
@@ -51,6 +56,7 @@ int memcmp(const void* s1, const void* s2, size_t n){
   }
   if(*(char *)s1-*(char *)s2<0)return -1;
   return *(char *)s1-*(char *)s2;
+  return memcmp(s1,s2,n);
 }
 
 #endif
