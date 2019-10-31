877def34ed340abe05b73c6c7c58850096bd4350
tracer-ics2019 <tracer@njuics.org>
2019-10-31 15:52:45 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:52:45 up  1:43,  1 user,  load average: 0.80, 0.74, 0.76 941adea9ad0cf704b908f5f4cfef824b7dbdb88

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -5,14 +5,23 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(ref_r->eax!=reg_l(R_EAX)||ref_r->ebx!=reg_l(R_EBX)||ref_r->ecx!=reg_l(R_ECX)
   ||ref_r->edx!=reg_l(R_EDX)||ref_r->ebp!=reg_l(R_EBP)||ref_r->esi!=reg_l(R_ESI)||
   ref_r->edi!=reg_l(R_EDI)||ref_r->pc!=cpu.pc){
+    if(ref_r->eax!=cpu.ecx)printf("->");
     printf("r.eax: %-8x  cpu.eax: %-8x\n",ref_r->eax,cpu.eax);
+    if(ref_r->ecx!=cpu.ecx)printf("->");
     printf("r.ecx: %-8x  cpu.ecx: %-8x\n",ref_r->ecx,cpu.ecx);
+    if(ref_r->edx!=cpu.edx)printf("->");
     printf("r.edx: %-8x  cpu.edx: %-8x\n",ref_r->edx,cpu.edx);
+    if(ref_r->ebx!=cpu.ebx)printf("->");
     printf("r.ebx: %-8x  cpu.ebx: %-8x\n",ref_r->ebx,cpu.ebx);
+    if(ref_r->esp!=cpu.esp)printf("->");
     printf("r.esp: %-8x  cpu.esp: %-8x\n",ref_r->esp,cpu.esp);
+    if(ref_r->ebp!=cpu.ebp)printf("->");
     printf("r.ebp: %-8x  cpu.esp: %-8x\n",ref_r->ebp,cpu.ebp);
+    if(ref_r->esi!=cpu.esi)printf("->");
     printf("r.esi: %-8x  cpu.esi: %-8x\n",ref_r->esi,cpu.esi);
+    if(ref_r->edi!=cpu.edi)printf("->");
     printf("r.edi: %-8x  cpu.edi: %-8x\n",ref_r->edi,cpu.edi);
+    if(ref_r->pc!=cpu.pc)printf("->");
     printf("r.pc:  %-8x  cpu.pc:  %-8x\n",ref_r->pc,cpu.pc);
     return false;
   }
--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -27,8 +27,8 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   //TODO();
-  /*reg_l(R_ESP)=reg_l(R_ESP)-4;
-  vaddr_write(reg_l(R_ESP),*src1,4);*/
+  reg_l(R_ESP)=reg_l(R_ESP)-4;
+  vaddr_write(reg_l(R_ESP),*src1,4);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
