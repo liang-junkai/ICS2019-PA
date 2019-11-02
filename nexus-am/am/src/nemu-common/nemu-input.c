6720a02b4ce4412f8c02dfd7fe2335afa67ceda3
tracer-ics2019 <tracer@njuics.org>
2019-11-03 00:56:59 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  00:56:59 up 10:13,  1 user,  load average: 0.39, 0.48, 0.52 d8b65b8fb76f2f72f6e13c2342f98e8c47a81e2f

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -1,16 +1,16 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
 #define KEYDOWN_MASK 0x8000
 
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      
+      uint32_t code=0;
+      code=inl(KBD_ADDR);
       kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      kbd->keycode = code;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
