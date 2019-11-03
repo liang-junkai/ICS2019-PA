bd7c4146984ce863f595c516ccbe0e0e948c09d2
tracer-ics2019 <tracer@njuics.org>
2019-11-03 17:01:35 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:01:35 up 20 min,  1 user,  load average: 0.57, 0.48, 0.49 db30f0ec9bd23107797b63f57083f50da00a1788

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,24 +1,35 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
+#include<string.h>
+#define W 400
+#define H 300
+static uint32_t fb[W * H] = {};
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = W;
+      info->height = H;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
   return 0;
 }
-
 size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+      int x=ctl->x,y=ctl->y,w=ctl->w,h=ctl->h;
+      uint32_t *pixels=ctl->pixels;
+      int cp_bytes=sizeof(uint32_t) * min(w, W - x);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
