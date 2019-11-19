0ee139d711ddf498d81e67c744781c5ec6c9c098
tracer-ics2019 <tracer@njuics.org>
2019-11-19 19:57:58 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:57:58 up  1:06,  1 user,  load average: 1.74, 1.17, 1.05 538b690cba88595827e9280024855e78de9bb754

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -4,6 +4,15 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+   rtl_push(&cpu.EFLAGS.value);
+   rtl_push(&cpu.cs);
+   rtl_push(&ret_addr);
+   assert(NO<=cpu.idtr.limit);
+   vaddr_t gate_addr=cpu.idtr.base+8*NO;
+   uint32_t low,high;
+   low=vaddr_read(gate_addr,4)&0x0000ffff;
+   high=vaddr_read(gate_addr+4,4)&0xffff0000;
+   rtl_j(low|high);
 }
 
 bool isa_query_intr(void) {
