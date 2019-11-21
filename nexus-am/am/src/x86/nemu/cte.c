b447f75f2ab1b7e3a6b92acd00b22af6852aa617
tracer-ics2019 <tracer@njuics.org>
2019-11-21 14:55:07 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:55:07 up 35 min,  1 user,  load average: 1.13, 1.07, 0.94 9b6802a72e9a4928c7176be4b21e2491c7ae926

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -1,17 +1,19 @@
 #include <am.h>
 #include <x86.h>
-
+//#include<stdio.h>
+#include<klib.h>
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 void __am_irq0();
 void __am_vecsys();
 void __am_vectrap();
 void __am_vecnull();
-
+//extern int printf(const char *fmt,...);
 _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
   if (user_handler) {
     _Event ev = {0};
+    printf("yield irq: %d\n",c->irq);
     switch (c->irq) {
       default: ev.event = _EVENT_ERROR; break;
     }
