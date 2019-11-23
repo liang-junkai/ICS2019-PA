bc39675550ee0d224679a22666895aae5ae24778
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:07:26 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:07:26 up  2:20,  1 user,  load average: 0.95, 0.71, 0.69 80e445b8349f214b03e027fca8cd72c25f96c6

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,10 @@
 #include "common.h"
 #include "syscall.h"
-
+static inline uintptr_t sys_yield(_Context *c){
+  _yield();
+  c->GPRx=0;
+  return 1;
+}
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -8,7 +12,7 @@ _Context* do_syscall(_Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   switch (a[0]) {
-    case 1: _yield();break;
+    case 1: sys_yield(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
