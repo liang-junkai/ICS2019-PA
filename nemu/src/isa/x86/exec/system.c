6bc3719be458dda1dd11b8f63842ae636267669a
tracer-ics2019 <tracer@njuics.org>
2019-11-23 16:49:58 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:49:58 up  2:02,  1 user,  load average: 0.95, 0.64, 0.58 a09e22216b11a09a3208ed2fb6750a8c9a13b534

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -29,6 +29,7 @@ make_EHelper(mov_cr2r) {
 void raise_intr(uint32_t NO, vaddr_t ret_addr);
 make_EHelper(int) {
   //TODO();
+  printf("decinfo.seq_pc: %x\n",decinfo.seq_pc);
   raise_intr(id_dest->val,decinfo.seq_pc);
   print_asm("int %s", id_dest->str);
 
