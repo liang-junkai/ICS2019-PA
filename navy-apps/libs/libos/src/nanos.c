ab076a77c72413a502568c606d2b3a2a9626c16d
tracer-ics2019 <tracer@njuics.org>
2019-11-25 14:49:23 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:49:23 up 20 min,  1 user,  load average: 0.81, 1.12, 0.88 c5de9dcd4548bc7ef4f413496643d39c3030c49c

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -61,6 +61,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
+  printf("reached write\n");
   _exit(SYS_write);
   //_syscall_(SYS_write,(intptr_t)buf,count);
   return 0;
