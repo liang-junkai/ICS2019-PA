93325b05088a080acc8abcac557913b178a4090b
tracer-ics2019 <tracer@njuics.org>
2019-11-25 23:27:41 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:27:41 up 4 min,  1 user,  load average: 1.34, 1.12, 0.53 5f52fa58e939eb06fa08262cf30e16c210e22e3f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -14,7 +14,7 @@ size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   ramdisk_read((void *)DEFAULT_ENTRY,0,get_ramdisk_size());
-  return (uintptr_t)(DEFAULT_ENTRY+0x10fc);
+  return (uintptr_t)(DEFAULT_ENTRY+0x100c);
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
