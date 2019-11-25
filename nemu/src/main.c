b83a3fc478df78535e7ab59bf21785791f30aa09
tracer-ics2019 <tracer@njuics.org>
2019-11-25 18:36:51 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:36:51 up  4:07,  1 user,  load average: 1.50, 1.00, 0.82 2786b6b094516285a7191b98879351a0db13db6f

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   /* Receive commands from user. */
-  //is_batch_mode=0;
+  is_batch_mode=0;
   ui_mainloop(is_batch_mode);
 
   return 0;
