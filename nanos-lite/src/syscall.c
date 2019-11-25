9c7224de06000dd2ba513402eed7f3459b7add51
tracer-ics2019 <tracer@njuics.org>
2019-11-25 18:53:31 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:53:31 up  4:24,  1 user,  load average: 1.10, 0.91, 0.86 fb62cd275cd455fda62e30971ca943a5058582f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -38,7 +38,7 @@ _Context* do_syscall(_Context *c) {
     case SYS_exit: sys_exit(c);break;
     case SYS_yield: sys_yield(c);break;
     case SYS_write: sys_write(c,a[1],a[2],a[3]);break;
-    case SYS_execve: sys_execve(c);break;
+    //case SYS_execve: sys_execve(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   _Context *next=__am_irq_handle(c);
