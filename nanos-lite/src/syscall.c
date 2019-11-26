5f4d57a3dfbd6f8b87c800f4a616af07e18982a8
tracer-ics2019 <tracer@njuics.org>
2019-11-26 18:01:08 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:01:08 up  1:17,  1 user,  load average: 0.55, 0.66, 0.60 81f74db4286f0d2a59a7d4c774d4214d10eb68

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,9 +29,9 @@ void sys_execve(_Context *c){
 _Context* __am_irq_handle(_Context *c);
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR2;
-  a[1] = c->GPR3;
-  a[2] = c->GPR2;
+  a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
   a[3] = c->GPR4;
   printf("GPR1: %x GPR2: %x GPR3: 0x%x GPR4: %x GPRx: %x\n",c->GPR1,c->GPR2,c->GPR3,c->GPR4,c->GPRx);
   switch (a[0]) {
