c111f59327b6f6d122f41d002eb6c87641657586
tracer-ics2019 <tracer@njuics.org>
2019-12-02 17:06:44 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:06:44 up  2:40,  1 user,  load average: 1.22, 1.02, 0.78 bcce5eede836514ab1becd90c8540f3d0cda633

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -4,18 +4,18 @@
 int main() {
   FILE *fp = fopen("/share/texts/num", "r+");
   assert(fp);
-
+  printf("1\n");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
+  printf("2\n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("3\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
@@ -25,13 +25,13 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("4\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-
+  printf("5\n");
   fclose(fp);
 
   printf("PASS!!!\n");
