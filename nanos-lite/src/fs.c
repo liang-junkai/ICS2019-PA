2d92edc031df46299ded06f76e1fba68af9c88b8
tracer-ics2019 <tracer@njuics.org>
2019-12-02 20:01:37 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:01:37 up  5:34,  1 user,  load average: 0.80, 0.80, 0.68 f8804ae8a43f07d3ff0a234edeaeb744be4c9967

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,8 +29,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, invalid_read, invalid_write,0},
-  {"stdout", 0, 0, invalid_read, serial_write,0},
-  {"stderr", 0, 0, invalid_read, serial_write,0},
+  {"stdout", -1, 0, invalid_read, serial_write,0},
+  {"stderr", -1, 0, invalid_read, serial_write,0},
 #include "files.h"
 };
 
