a0c6d5a212b7f83c911873e311ceda1b7a7dce09
tracer-ics2019 <tracer@njuics.org>
2019-12-02 19:27:23 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:27:23 up  5:00,  1 user,  load average: 0.67, 0.91, 0.92 aebab63b154c7ef4613ada87db841fce1ee6e63f

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -3,22 +3,22 @@
 #include<unistd.h>
 
 int main() {
-  printf("ljk\n");
+  //printf("ljk\n");
   FILE *fp = fopen("/share/texts/num", "r+");
-  printf("0\n");
+  //printf("0\n");
   assert(fp);
-  printf("1\n");
+  //printf("1\n");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-  printf("2\n");
+  //printf("2\n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  printf("3\n");
+  //printf("3\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
