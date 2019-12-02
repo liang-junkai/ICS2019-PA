0e75a0090262a13f96cb06f5bcbbc7594944dbec
tracer-ics2019 <tracer@njuics.org>
2019-12-02 17:18:43 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:18:43 up  2:52,  1 user,  load average: 1.59, 1.16, 0.98 b83bb85df8ee97b31390cab248be7dda21881d06

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -3,18 +3,18 @@
 
 int main() {
   FILE *fp = fopen("/share/texts/num", "r+");
-  //printf("0\n");
+  printf("0\n");
   assert(fp);
-  //printf("1\n");
+  printf("1\n");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
-  //assert(size == 5000);
+  assert(size == 5000);
   //printf("2\n");
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    //assert(n == i + 1);
+    assert(n == i + 1);
   }
   //printf("3\n");
   fseek(fp, 0, SEEK_SET);
@@ -24,13 +24,13 @@ int main() {
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    //assert(n == i + 1);
+    assert(n == i + 1);
   }
   //printf("4\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
-    //assert(n == i + 1 + 1000);
+    assert(n == i + 1 + 1000);
   }
   //printf("5\n");
   fclose(fp);
