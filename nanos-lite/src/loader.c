c76816e61ac5d70922c44088b888242ad22ec26a
tracer-ics2019 <tracer@njuics.org>
2019-12-02 20:53:16 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:53:16 up  6:26,  1 user,  load average: 0.58, 0.59, 0.67 a2f057a1f9dc5a79c52c581c3afd098e728222ed

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -40,6 +40,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr phdr;
   for(int i=0;i<n;i++){
     ramdisk_read(&phdr,ehdr.e_phoff+sizeof(Elf_Phdr)*i+fs_diskset(fd),sizeof(Elf_Phdr));
+    printf("%d\n",i);
     if(phdr.p_type==PT_LOAD){
       ramdisk_read((void*)phdr.p_vaddr,phdr.p_offset+fs_diskset(fd),phdr.p_memsz);
       memset((void*)(phdr.p_vaddr+phdr.p_filesz),0,phdr.p_memsz-phdr.p_filesz);
