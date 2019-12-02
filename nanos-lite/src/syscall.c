1141a3da2431e039fb3d95185b96afecf0cbe641
tracer-ics2019 <tracer@njuics.org>
2019-12-02 19:22:39 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:22:39 up  4:56,  1 user,  load average: 0.79, 0.87, 0.90 49d4c656911c90f5b8ed0f1fb1093091b77da12b

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -30,6 +30,18 @@ void sys_execve(_Context *c){
 void sys_brk(_Context*c){
   c->GPRx=0;
 }
+size_t fs_open(const char* pathname,int flags,int mode);
+void sys_open(_Context*c){
+  c->GPRx=fs_open((char*)c->GPR2,c->GPR3,c->GPR4);
+}
+size_t fs_close(int fd);
+void sys_close(_Context*c){
+  c->GPRx=fs_close(c->GPR2);
+}
+size_t fs_lseek(int fd,size_t offset,int whence);
+void sys_lseek(_Context*c){
+  c->GPRx=fs_lseek(c->GPR2,c->GPR3,c->GPR4);
+}
 _Context* __am_irq_handle(_Context *c);
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -42,6 +54,9 @@ _Context* do_syscall(_Context *c) {
     case SYS_exit: sys_exit(c);break;
     case SYS_yield: sys_yield(c);break;
     case SYS_write: sys_write(c,c->GPR2,c->GPR3,c->GPR4);break;
+    case SYS_open: sys_open(c);break;
+    case SYS_close: sys_close(c);break;
+    case SYS_lseek: sys_lseek(c);break;
     case SYS_brk:sys_brk(c);break;
     //case SYS_execve: sys_execve(c);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
