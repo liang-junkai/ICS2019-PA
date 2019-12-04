16fda3b7b3dbdd0513dfe66006f19e7ba36bbcf4
tracer-ics2019 <tracer@njuics.org>
2019-12-04 19:20:14 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:20:14 up  1:26,  1 user,  load average: 0.93, 0.93, 1.05 27cc70e1f4124265fb3426f5fcd098b26bf0ad7c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,19 +16,31 @@ static const char *keyname[256] __attribute__((used)) = {
   [_KEY_NONE] = "NONE",
   _KEYS(NAME)
 };
-
 size_t events_read(void *buf, size_t offset, size_t len) {
   int key=read_key();
+  int down =0;
+	if (key & 0x8000) {
+		key ^= 0x8000;
+		down = 1;
+	}
+	if (key == _KEY_NONE) {
+		unsigned long t = uptime();
+		sprintf(buf, "t %d\n", t);
+	}
+	else {
+		sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
+	}
+	return strlen(buf);
   //printf("%d\n",key);
   //printf("%d\n",len);
   //return sprintf(buf,"t %d\n",uptime());
-  if(key!=0)printf("key\n");
+  /*if(key!=0)printf("key\n");
   if(key==0){
     return sprintf(buf,"t %d\n",uptime());
   }else{
     //printf("hit\n");
     return sprintf(buf,"k%c %s\n",key&0x8000?'u':'d',keyname[key&0x7fff]);
-  }
+  }*/
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
