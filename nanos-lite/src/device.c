0f0a297f796774e68dc289bc88883d2acbfdf093
tracer-ics2019 <tracer@njuics.org>
2019-12-04 18:28:56 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:28:56 up 35 min,  1 user,  load average: 0.70, 0.79, 0.77 7a0b0e20b8b5e2c188cca105ad9d1ccd2725e8c7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -18,13 +18,14 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  int key=read_key();
-  printf("%d\n",key);
-  if(key==0){
+  //int key=read_key();
+  //printf("%d\n",key);
+  return sprintf(buf,"t %d\n",uptime());
+  /*if(key==0){
     return snprintf(buf,len, "t %d\n",uptime());
   }else{
     return snprintf(buf,len, "k%c %s\n",key&0x8000?'u':'d',keyname[key&0x7fff]);
-  }
+  }*/
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
