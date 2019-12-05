f5485ab6c416238fb4053c6b9f14b5f546543268
tracer-ics2019 <tracer@njuics.org>
2019-12-05 23:08:12 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:08:12 up 48 min,  1 user,  load average: 1.09, 0.99, 0.98 5fbbb7bcb363943ab0f52f049f5879feef07a47

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -11,7 +11,8 @@ extern uint8_t ramdisk_end;
 
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
-  //assert(offset + len <= RAMDISK_SIZE);
+  printf("%x %x\n",offset,len);
+  assert(offset + len <= RAMDISK_SIZE);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
 }
