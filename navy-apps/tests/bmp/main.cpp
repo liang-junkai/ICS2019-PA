81ae5a9fe5a37cb2f01150340e6b697e38a913b5
tracer-ics2019 <tracer@njuics.org>
2019-12-05 17:20:19 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:20:19 up  1:24,  1 user,  load average: 0.96, 0.85, 0.91 94dbbb47b9420b941e3e484316d9b2d63db959a4

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -6,7 +6,7 @@ int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
   printf("aa0\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  //printf("aa1\n");
+  printf("aa1\n");
   assert(bmp->pixels);
   //printf("aa\n");
   NDL_OpenDisplay(bmp->w, bmp->h);
