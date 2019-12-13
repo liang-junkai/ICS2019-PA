1da60b3fa21246b322c89ba04cd91e4281dd3f40
tracer-ics2019 <tracer@njuics.org>
2019-12-13 20:45:42 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:45:42 up  9:25,  1 user,  load average: 0.84, 0.87, 0.76 453e6b2dcb0054356dbb6d67ae3d7a54a5b10ffb

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,10 +2,16 @@
 _Context* do_syscall(_Context *c);
 _Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
-  printf("do_event\n");
+  //printf("do_event\n");
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(c);break;
-    case _EVENT_YIELD: printf("you have reached _EVENT_YIELD\n");return schedule(c);break;
+    case _EVENT_YIELD: {
+      printf("you have reached _EVENT_YIELD\n");
+      _Context *c2=schedule(c);
+      printf("c2: 0x%x\n",c2->eip);
+      return c2;
+      break;
+    }
     default: panic("Unhandled event ID = %d", e.event);
   }
   //printf("do_event c:%d\n",c->GPRx);
