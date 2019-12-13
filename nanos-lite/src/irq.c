56a9e1a9af31901d7b278d1c28061e52d5ed8807
tracer-ics2019 <tracer@njuics.org>
2019-12-13 19:44:50 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:44:50 up  8:24,  1 user,  load average: 0.62, 0.59, 0.57 a4c6a0291b92192aecdc9a15459497afd0d91e4

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,10 +1,11 @@
 #include "common.h"
 _Context* do_syscall(_Context *c);
+_Context* schedule(_Context *prev);
 static _Context* do_event(_Event e, _Context* c) {
   //printf("do_event\n");
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(c);break;
-    case _EVENT_YIELD: printf("you have reached _EVENT_YIELD\n");break;
+    case _EVENT_YIELD: printf("you have reached _EVENT_YIELD\n");return schedule(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   //printf("do_event c:%d\n",c->GPRx);
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -5,7 +5,7 @@
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
-
+static int fg_pcb=1;
 void switch_boot_pcb() {
   current = &pcb_boot;
 }
@@ -18,7 +18,9 @@ void hello_fun(void *arg) {
     _yield();
   }
 }
+void context_kload(PCB *pcb, void *entry);
 void naive_uload(PCB *pcb, const char *filename);
+void context_uload(PCB *pcb, const char *filename) ;
 void init_proc() {
   //switch_boot_pcb();
 
@@ -29,10 +31,39 @@ void init_proc() {
   //naive_uload(NULL,"/bin/init");
 
 }
-
+char *program[]={
+  "/bin/hello",
+  "/bin/pal",
+  "/bin/dummy",//For exit
+  "/bin/init"
+};
+size_t fs_read(int fd,void *buf,size_t len);
+size_t fs_open(const char* pathname,int flags,int mode);
 _Context* schedule(_Context *prev) {
-  //current->cp=prev;
-  //current=&pcb[0];
-  //return current->cp;
-  return NULL;
+  current->cp=prev;
+  current=&pcb[0];
+  static int fd=-1;
+  if(fd==-1){
+    fd=fs_open("/dev/events",0,0);
+  }
+  char info[25],con[10]="kd F\0\0";
+  fs_read(fd,info,25);
+  int i;
+  for(i=1;i<5;++i){
+    con[4]='0'+i;//4 for "kd F"
+    if(!strncmp(info,con,5)){
+      switch(i){
+          case 1:case 2:case 3:
+            printf("switch between hello and %s\n",program[i]+5);//+5 for "/bin/"
+            fg_pcb=i;
+            break;
+          case 4:
+            printf("reload pcb[0]\n");
+            context_uload(&pcb[0], program[0]);
+            break;
+        }
+        break;
+      }
+    }
+    return current->cp;
 }
