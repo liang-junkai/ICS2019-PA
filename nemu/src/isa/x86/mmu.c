ab3480d855dc65f89554dd7860f35f2889f2d84a
tracer-ics2019 <tracer@njuics.org>
2019-12-28 22:47:38 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:47:38 up  1:17,  1 user,  load average: 0.66, 0.39, 0.43 fb39097d314fc28b87849c04ac71f351bcbf99d

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,9 +1,42 @@
 #include "nemu.h"
-
+#include"isa/mmu.h"
+static paddr_t page_translate(vaddr_t addr);
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if((addr>>12)!=(addr+len-1)>>12){
+    assert(0);
+  }
+  else{
+    paddr_t paddr=page_translate(addr);
+    return paddr_read(paddr, len);
+  }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  if((addr>>12)!=(addr+len-1)>>12){
+    assert(0);
+  }
+  else{
+    paddr_t paddr=page_translate(addr);
+    paddr_write(paddr, data, len);
+  }
+}
+static paddr_t page_translate(vaddr_t addr){
+  CR0 cr0;
+  cr0.val=cpu.CR0;
+  if(cr0.paging){
+    uint32_t addr_dir=addr>>22;
+    uint32_t addr_page=(addr<<10)>>22;
+    uint32_t addr_offset=(addr<<20)>>20;
+    paddr_t PDE_addr=((cpu.CR3>>12)<<12)|(addr_dir<<2);
+    PDE pde;
+    PDE pte;
+    pde.val=paddr_read(PDE_addr,4);
+    assert(pde.present);
+    paddr_t PTE_addr=(pde.page_frame<<12)|(addr_page<<2);
+    pte.val=paddr_read(PTE_addr,4);
+    assert(pte.present);
+    paddr_t paddr=(pte.page_frame<<12)|(addr_offset);
+    return paddr;
+  }
+  return addr;
 }
