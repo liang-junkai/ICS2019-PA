759b77baefcb50c7ac6df846df41734dd94faf67
tracer-ics2019 <tracer@njuics.org>
2020-01-06 19:06:21 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:06:21 up  1:41,  1 user,  load average: 0.34, 0.53, 0.60 fc68c063b286aad7e0f081823f4478b8a088214

--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -108,7 +108,7 @@ _Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack, void *entry, 
 //_kcontext
   _Context *c=(_Context*)(ustack.end)-1;
 
-  *(_Context**)(ustack.start)=c;
+  // *(_Context**)(ustack.start)=c;
   c->eip=(uintptr_t)entry;
   c->edi=0;
   c->eflags=(c->eflags|1<<9);
