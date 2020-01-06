310c943adcb0ea02b80b750c8851101144006aaf
tracer-ics2019 <tracer@njuics.org>
2020-01-06 22:07:44 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:07:44 up  4:42,  1 user,  load average: 0.81, 0.61, 0.46 95cf047a9824887fd73b24f8521cbccb468dc78d

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -14,14 +14,33 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
-
+  //TODO();
+  switch(id_dest->reg){
+    case 0:
+      cpu.CR0=id_src->val;
+      break;
+    case 3:
+      cpu.CR3=id_src->val;
+      break;
+    default:
+      Assert(0,"Should not reach here (cr%d)",id_dest->reg);
+  }
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
-
+  //TODO();
+  switch(id_src->reg){
+    case 0:
+      operand_write(id_dest,&cpu.CR0);
+      break;
+    case 3:
+      operand_write(id_dest,&cpu.CR3);
+      break;
+    default:
+      Assert(0,"Should not reach here (cr%d)",id_dest->reg);
+      
+  }
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
   difftest_skip_ref();
