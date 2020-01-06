a01c2257fa4b4fbbf9be3bbad1598abaa6cf46d4
tracer-ics2019 <tracer@njuics.org>
2020-01-06 18:46:54 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:46:54 up  1:21,  1 user,  load average: 0.60, 0.77, 0.71 a320b22df9423ac6685145548f6eb81583e94f8d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,6 +8,8 @@
 #else
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
+# define vaddr_t uint32_t
+# define paddr_t uint32_t
 #endif
 #define DEFAULT_ENTRY 0x3000000
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
@@ -15,6 +17,8 @@ size_t get_ramdisk_size();
 size_t fs_read(int fd,void *buf,size_t len);
 size_t fs_openset(int fd);
 size_t fs_diskset(int fd);
+size_t fs_close(int fd);
+size_t fs_lseek(int fd, size_t offset, int whence);
 size_t fs_open(const char* pathname,int flags,int mode);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t fs_read2(int fd,void *buf,size_t len);
@@ -32,7 +36,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //printf("ramdisksize: %x\n",get_ramdisk_size());
   //printf("elf.entry: %d\n",elf_e->e_entry);
   //return (uintptr_t)(DEFAULT_ENTRY+0x10fc);
-  Elf_Ehdr ehdr;
+  /*Elf_Ehdr ehdr;
   size_t fd=fs_open(filename,0,0);
   size_t size=fs_read(fd,&ehdr,sizeof(Elf_Ehdr));
   size++;
@@ -45,7 +49,75 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       memset((void*)(phdr.p_vaddr+phdr.p_filesz),0,phdr.p_memsz-phdr.p_filesz);
     }
   }
-  return ehdr.e_entry;
+  return ehdr.e_entry;*/
+  int fd=fs_open(filename,0,0);
+  Elf_Ehdr header;
+  fs_read(fd,(void*)&header,sizeof(header));
+  Elf_Phdr phdr[header.e_phnum];
+  char buf[4096];
+  fs_lseek(fd, header.e_phoff, 0);
+  fs_read(fd, (void*) &phdr, header.e_phnum*header.e_phentsize);
+  for (int index=0; index<header.e_phnum; index++) {
+    if (phdr[index].p_type != PT_LOAD) 
+      continue;
+    fs_lseek(fd, phdr[index].p_offset, 0);
+    int left = phdr[index].p_filesz;
+    int last_read = 0;
+    //void *va=DEFAULT_ENTRY;
+    //void *pa=DEFAULT_ENTRY;
+    vaddr_t va=0;
+    paddr_t pa=0;
+    while (left>0) {
+      if (left>=4096){
+        fs_read(fd, (void*) buf, 4096);
+        va = (phdr[index].p_vaddr+phdr[index].p_filesz-left);
+        pa = (paddr_t)new_page(1);
+        // Log("set map va=%d, pa=%d\n", va, pa);
+        _map(&pcb->as, (void*)va, (void*)pa, 1);
+        memcpy((void*)pa, buf, 4096);
+        left-=4096;
+        last_read = 4096;
+      }
+      else {
+        fs_read(fd, (void*) buf, left);
+        va = (phdr[index].p_vaddr+phdr[index].p_filesz-left);
+        pa = (paddr_t)new_page(1);
+        // Log("set map va=%d, pa=%d\n", va, pa);
+        _map(&pcb->as, (void*)va, (void*)pa, 1);
+        memcpy((void*)pa, buf, left);
+        last_read = left;
+        left-=left;
+      }
+    }
+    left = phdr[index].p_memsz-phdr[index].p_filesz;
+    if (last_read!=4096) {
+      va = (phdr[index].p_vaddr+phdr[index].p_filesz);
+      int res = 4096-last_read;
+      if (res>=left) {
+        memset((void*)(pa+last_read), 0, left);
+        continue;
+      }
+      else {
+        memset((void*)(pa+last_read), 0, res);
+        left-=res;
+      }
+      while (left>0) {
+        va = (phdr[index].p_vaddr + phdr[index].p_memsz-left);
+        pa = (paddr_t)new_page(1);
+        _map(&pcb->as, (void*)va, (void*)pa, 1);
+        if (left>=4096) {
+          memset((void*)pa, 0, 4096);
+          left-=4096;
+        }
+        else {
+          memset((void*)pa, 0, left);
+          left-=left;
+        }
+      } 
+    }
+  }
+  fs_close(fd);
+  return header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
