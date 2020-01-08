6c0a8d3fbe5b634fb019bbb07afdf8ba06d757a5
tracer-ics2019 <tracer@njuics.org>
2020-01-08 15:14:08 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:14:08 up 57 min,  1 user,  load average: 1.17, 0.96, 0.92 f239837418738c4ff14a420c800026fbd4ea0

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,10 +24,13 @@ void context_uload(PCB *pcb, const char *filename) ;
 void init_proc() {
   //switch_boot_pcb();
 
-  //Log("Initializing processes...");
+  Log("Initializing processes...");
   //context_kload(&pcb[0], (void *)hello_fun);
   //context_uload(&pcb[1],"/bin/init");
   context_uload(&pcb[0],"/bin/pal");
+  context_uload(&pcb[1],"/bin/hello");
+  context_uload(&pcb[2],"/bin/pal");
+  context_uload(&pcb[3],"/bin/pal");
   switch_boot_pcb();
   // load program here
   //naive_uload(NULL,"/bin/init");
