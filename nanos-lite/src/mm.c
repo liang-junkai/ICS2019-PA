5915465b7d2e2fa5cb4bd1deced8368d4c525165
tracer-ics2019 <tracer@njuics.org>
2020-01-08 17:34:33 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-38-generic #41-Ubuntu SMP Tue Dec 3 00:27:35 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:34:33 up  2:01,  1 user,  load average: 1.59, 1.24, 1.11 be95e1a7884251352aaf8b21e1bef7614c1e5088

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -22,8 +22,8 @@ int mm_brk(uintptr_t brk, intptr_t increment) {
   if(brk+increment>current->max_brk){
     void *va=(void*)((current->max_brk/4096)<<12);
     int left=brk+increment-current->max_brk;
-    int res = (4096-(current->max_brk%4096))%4096;
-    left = left-res;
+    //int res = (4096-(current->max_brk%4096))%4096;
+    //left = left-res;
     while(left>0){
       _map(&current->as,(void*)va,new_page(1),1);
       va+=PGSIZE;
