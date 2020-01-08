3906f845a9b51df12a47259290ae823319c05880
tracer-ics2019 <tracer@njuics.org>
2020-01-08 15:10:55 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:10:55 up 54 min,  1 user,  load average: 0.66, 0.83, 0.88 8d12b4b4a9d00c59e889925e88718d48fce82f2a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,10 +8,10 @@
 #else
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
-# define vaddr_t uint32_t
-# define paddr_t uint32_t
 #endif
 #define DEFAULT_ENTRY 0x3000000
+# define vaddr_t uint32_t
+# define paddr_t uint32_t
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 size_t fs_read(int fd,void *buf,size_t len);
