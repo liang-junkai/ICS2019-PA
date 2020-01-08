864f49b6ca835cd38518caf8353eb02299662df1
tracer-ics2019 <tracer@njuics.org>
2020-01-08 14:55:38 +0800

    >  compile 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-37-generic #40-Ubuntu SMP Thu Nov 14 00:14:01 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:55:38 up 38 min,  1 user,  load average: 1.34, 1.23, 1.02 967764a0e147d86fec66af58bc5a16191eea15be

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -45,7 +45,7 @@ static paddr_t page_translate(vaddr_t addr){
     assert(pde.present);
     paddr_t PTE_addr=(pde.page_frame<<12)|(addr_page<<2);
     pte.val=paddr_read(PTE_addr,4);
-    //assert(pte.present);
+    assert(pte.present);
     paddr_t paddr=(pte.page_frame<<12)|(addr_offset);
     //printf("0x%x\n",paddr);
     return paddr;
