3f4e6a23ced4c439b5becab7be0b82d34437b83a
tracer-ics2019 <tracer@njuics.org>
2020-01-08 18:24:01 +0800

    >  run 181840135 ljk Linux ljk-ThinkPad-13 5.0.0-38-generic #41-Ubuntu SMP Tue Dec 3 00:27:35 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:24:01 up  2:51,  1 user,  load average: 1.03, 0.77, 0.67 c78f1030a764d2261cdc166ef725b7f913fba9c5

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,7 @@ static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_SYSCALL: do_syscall(c);break;
     case _EVENT_YIELD: {
-      printf("you have reached _EVENT_YIELD\n");
+      //printf("you have reached _EVENT_YIELD\n");
       _Context *c2=schedule(c);
       //printf("c2: 0x%x\n",c2->eip);
       return c2;
